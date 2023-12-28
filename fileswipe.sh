ccd23d28c72cf7584dcea9161949078573a3f9bc
Barnaba Mateusz <barnaba@200ms.net>
2023-12-28 23:14:13 +0100

    fileswipe-run added + fileswipe.sh improvments

--- fileswipe.sh
+++ fileswipe.sh
@@ -23,27 +23,22 @@
 
 _FILE_REMOVE_CMD="rm"
 
-[ -n "$PRETEND" ] && [[ $(echo "$PRETEND" | tr '[:upper:]' '[:lower:]') =~ ^y|yes|1|on$ ]] && \
-        EXEC_RM='echo rm[pretend]' || EXEC_RM=$_FILE_REMOVE_CMD
-
 [ -n "$DEBUG" ] && [[ $(echo "$DEBUG" | tr '[:upper:]' '[:lower:]') =~ ^y|yes|1|on$ ]] && \
         set -xe || set -e
 
+# also '--pretend' parameter can be used
+[ -n "$PRETEND" ] && [[ $(echo "$PRETEND" | tr '[:upper:]' '[:lower:]') =~ ^y|yes|1|on$ ]] && \
+        EXEC_RM='echo rm[pretend]' || EXEC_RM=$_FILE_REMOVE_CMD
+
+
 OUTPUT="echo"
-# silent mode
-# OUTPUT="echo 1>/tmp/test"
+
 
 if [ "$_FILE_REMOVE_CMD" != "$EXEC_RM" ]; then
 	echo "Pretend mode ON"
 fi
 
 MARK_DIR='/var/spool/fileswipe.timemark/'
-
-SWIPE_DEFER_DAYS=7
-
-SWIPE_DEFER_SEC=$(($SWIPE_DEFER_DAYS*24*60*60))
-KEEP_MIN_CNT=$(($SWIPE_DEFER_DAYS*4)) # 7 * 4
-TIME_LOCK_TEST_MARGIN=$((60*60+3)) # one hour and 3 sec.
 
 OPT_FORCE=0
 
@@ -54,9 +49,27 @@ print_helpmsg_and_exit () {
 	fi
 
 	cat << EOF
-$(basename $0) [--force|-f] <dir path>
+$(basename $0) [--force|-f --pretend|-p] <dir path> <defer time> <rm freq.>
 	selectively remove files from <dir path> location
-	--force|-f - force selective removal, ignore time lock
+	--force|-f   - force selective removal, ignore time lock
+	--pretend|-p - don't remove just pretend
+	<defer time> - time within files removal is deferred, all files that
+	               are not old enough (specified by defer time) are not
+	               removed
+	<rm freq.>   - removal frequency of files after 'defer time' period
+	               is passed
+
+	Examples:
+	$(basename $0) /path/to/directory 1week 2
+	    remove every second file from '/path/to/directory' that is older
+	    then 1week
+
+	$(basename $0) /path/to/directory 7days 1
+	    remove files from '/path/to/directory' that is older then 7 days
+
+	$(basename $0) /path/to/directory 4weeks 3
+	    remove every third file from '/path/to/directory' that has passed
+	    one lunar cycle
 
 $(basename $0) --help|-h
 	print this help
@@ -67,19 +80,41 @@ EOF
 }
 
 
-[ -z $1 ] && print_helpmsg_and_exit "Missing dir. path"
+shopt -s extglob # enables extglob
 
-if [ $1 = "-h" ] || [ $1 = "--help" ]; then
-	print_helpmsg_and_exit
-elif [ $1 = "-f" ] || [ $1 = "--force" ]; then
-	OPT_FORCE=1
-	SWIPE_PATH=$2
-elif [ $(echo $1 | cut -c -1) = '-' ]; then
-	print_helpmsg_and_exit "Unknown option"
-else
-	SWIPE_PATH=$1
-fi
+for a in "$@"; do
 
+	case $a in
+		-h | --help)
+			print_helpmsg_and_exit
+		;;
+		-f | --force)
+			OPT_FORCE=1
+		;;
+		-p | --pretend)
+			EXEC_RM='echo rm[--pretend]'
+		;;
+		+([0-9])d?(ay)?(s))
+			SWIPE_DEFER_DAYS=$(echo $a | egrep -o '^[0-9]+')
+		;;
+		+([0-9])w?(eek)?(s))
+			SWIPE_DEFER_DAYS=$(($(echo $a | egrep -o '^[0-9]+')*7))
+		;;
+		+([0-9]))
+			RM_FREQ=$a
+		;;
+		*)
+			if [ $(echo $a | cut -c -1) = '-' ]; then
+				print_helpmsg_and_exit "Unknown option"
+			else
+				SWIPE_PATH=$a
+			fi
+	esac
+done
+
+shopt -u extglob
+
+[ -z $SWIPE_PATH ] && print_helpmsg_and_exit "Missing dir. path"
 
 if [ -d $SWIPE_PATH ]; then
 	# absolute directory path
@@ -87,6 +122,22 @@ if [ -d $SWIPE_PATH ]; then
 else
 	print_helpmsg_and_exit "Path is not pointing to directory"
 fi
+
+[ -z $SWIPE_DEFER_DAYS ] && print_helpmsg_and_exit "Provide all required arguments"
+if [ $SWIPE_DEFER_DAYS -lt 7 ] ; then
+	echo "File removal defer time must be at least 7days"
+	exit 2
+fi
+
+[ -z $RM_FREQ ] && print_helpmsg_and_exit "Provide all required arguments"
+if [ $RM_FREQ -lt 1 ] || [ $RM_FREQ -gt 16 ]; then
+	echo "Removal frequency must be between [1:16] range"
+	exit 2
+fi
+
+SWIPE_DEFER_SEC=$(($SWIPE_DEFER_DAYS*24*60*60))
+KEEP_MIN_CNT=$(($SWIPE_DEFER_DAYS*4)) # 7 * 4
+TIME_LOCK_TEST_MARGIN=$((65*60)) # one hour and 5 min.
 
 
 MARK_FILE=$MARK_DIR$(echo $ASWIPE_PATH | cut -c 2- | sed 's/\//_/g')
@@ -144,11 +195,11 @@ FOR_REMOVAL_CNT=0
 RM_LIST=""
 
 #date_integrity_chk_status=0
-parity=0
+parity=$(($RM_FREQ-1))
 for file in $(find "$SWIPE_PATH" -type f | sort); do
 	age_in_sec=$(($(date +%s) - $(date +%s -r "$file")))
 
-	if [ $age_in_sec -gt $SWIPE_DEFER_SEC ] && parity=$((($parity + 1)%2)) && [ $parity -eq 1 ]; then
+	if [ $age_in_sec -gt $SWIPE_DEFER_SEC ] && parity=$((($parity + 1)%$RM_FREQ)) && [ $parity -eq 0 ]; then
 		echo -n 'For removal '
 
 		FOR_REMOVAL_CNT=$(($FOR_REMOVAL_CNT+1))
